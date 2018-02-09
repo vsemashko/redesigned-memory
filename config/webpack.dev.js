21c4deee81daa17586ca8f403c8345cf2b6e047b
Vladimir Semashko <uladzimir.siamashka@virginholidays.com>
2018-02-09 10:00:31 +0300

    feat: add source-maps and missing dependencies

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -6,6 +6,7 @@ module.exports = {
     entry: {
         main: ['./src/main.js']
     },
+    devtool: 'source-map',
     output: {
         filename: '[name]-bundle.js',
         path: path.resolve(__dirname, '../dist'),
